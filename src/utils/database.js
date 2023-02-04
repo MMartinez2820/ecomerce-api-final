64277ac04e55640527d8822cc8a4b0a4e3c11a50
MMartinez2820 <201808724@p.uapa.edu.do>
2023-02-04 02:24:25 -0400

    cargando el render

--- src/utils/database.js
+++ src/utils/database.js
@@ -9,6 +9,9 @@ const db = new Sequelize({
   password: process.env.DB_PASSWORD,
   dialect: "postgres",
   logging: false,
+  //ojo esta linea se pone para poder correr en render, pero para poder correrlo aqui 
+  //como local hay que quitarlo. solo es para correr en el render
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 });
 
 module.exports = db;
